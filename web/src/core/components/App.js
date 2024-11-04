c4867010f949f556fc9e42c0bf44bc704b08e0f0
Ismael Anwarzai <ismaelanwarzai@Ismaels-MacBook-Air.local>
2024-11-03 22:41:29 -0500

    Update import paths for LoginPage.js in core/components

--- web/src/core/components/App.js
+++ web/src/core/components/App.js
@@ -5,9 +5,9 @@ import { Home, SOTD, About, Contact } from './modules';
 import { Navbar } from './core/components/Navbar';
 
 // Import the new Login, Signup, and Success components
-import LoginPage from 'web/src/core/components/LoginPage.js';
-import SignupPage from 'web/src/core/components/SignupPage.js';
-import SuccessPage from 'web/src/core/components/SuccessPage.js';
+import LoginPage from 'src/core/components/LoginPage.js';
+import SignupPage from 'src/core/components/SignupPage.js';
+import SuccessPage from 'src/core/components/SuccessPage.js';
 
 
 const App = () => {
