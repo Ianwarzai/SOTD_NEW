f97114d0150b7b6bdf0ea946f00f45e2425bb897
Ismael Anwarzai <ismaelanwarzai@Ismaels-MacBook-Air.local>
2024-11-05 23:44:24 -0500

    Fix import path for LoginPage in App.js

--- web/src/App.js
+++ web/src/App.js
@@ -5,9 +5,9 @@ import { Home, SOTD, About, Contact } from './modules';
 import { Navbar } from './core/components/Navbar';
 
 // Import the new Login, Signup, and Success components
-import LoginPage from './LoginPage';
-import SignupPage from './SignupPage';
-import SuccessPage from './SuccessPage';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
+import SuccessPage from './components/SuccessPage';
 
 
 const App = () => {
